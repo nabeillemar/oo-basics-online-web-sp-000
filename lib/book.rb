d5fa2688118272acea1047fdf4484c1af2988296
Nabeil Lemar <nabeillemar@gmail.com>
2020-09-19 20:20:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,35 +1,13 @@
-class Book 
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
   def initialize(title)
     @title = title
   end
-  
-  def title 
-    @title
-  end 
-  
-  def author=(author)
-    @author = author
-  end 
-  
-  def author 
-    @author
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-  
-  def page_count=(num)
-    @page_count = num 
-  end
-  
-  def page_count
-    @page_count
-  end 
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
-  
-  
-end 
+ 
+end
